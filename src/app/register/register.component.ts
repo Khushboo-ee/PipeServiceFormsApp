38deb5e887b7c6ff659a5fcb0d798b67a3a6dc13
Khushboo <khush23082012gmail.com>
2023-03-04 16:48:58 +0530

    added a sample registration form using validations

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  personname:string="";
+  gender:string="";
+  msg:string="";
+  pincode:number=0;
+  location:string = "";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ShowValues()
+  {
+      this.msg = "Name : " + this.personname + "<br />Gender : " + this.gender;
+      this.msg += "<br />Location : " + this.location + "<br />Pincode : " + this.pincode;
+  }
 }
